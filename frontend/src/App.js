cc42676be4d9660ecdc55ee63f4ad1ed896b7707
Harish Madineni <harishmadineni@Harishs-MacBook-Pro.local>
2023-09-24 04:30:39 +0530

    Added hello world react app.

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Card>Hello World</Card>
+    </React.Fragment>
+  );
+}
+
+export default App;
